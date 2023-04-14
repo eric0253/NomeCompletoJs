854b50ba741191d2f7aa988c67085e4da25c442e
Eric Bochiccia <77254171+eric0253@users.noreply.github.com>
2023-04-13 22:08:25 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+let Nome = prompt("Digite seu Primeiro Nome:")
+let SobreNome = prompt("Digite seu Sobrenome:")
+
+alert(`Seu nome completo é ${Nome} ${SobreNome}`)
+alert(`Seu nome de catálogo é ${SobreNome.toUpperCase()}, ${Nome}`) 
